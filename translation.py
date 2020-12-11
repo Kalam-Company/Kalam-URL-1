c6e04e53e6de9cdc3c68a382b8134f25b31fbe2a
Kiruthikvarma55 <75213972+Kiruthikvarma55@users.noreply.github.com>
2020-12-11 19:13:11 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Welcome my Friend,
-I am Telegram URL Uploader Bot with Custom Thumbnail support!!!
+     I am Telegram URL Uploader Bot with Custom Thumbnail support!!!
 
 <b>Please send me any direct download URL Link, I can upload to telegram as File/Video</b>
 
@@ -11,22 +11,21 @@ I am Telegram URL Uploader Bot with Custom Thumbnail support!!!
     ABS_TEXT = "To succeed in your mission, you must have a single-minded devotion to your goal."
     UPGRADE_TEXT = "Join to my Developer channel © @Kalam_Company for upgrade your plan...  /help for more Details"
     FORMAT_SELECTION = """Please Select the Desired Format 👇👇👇
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File with Screenshots
-   Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots 
 
-<b>If you want to Set custom Thumbnail, Send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail.</b>"""
-    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
-URL | filename | username | password"""
-    NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+   📸🎥 SVIDEO - Send as Streaming Video format with screenshots
+   🎥 VIDEO  - Send as Stream format without Screenshots
+   📸📁 SFILE  - Send as File format with Screenshots
+   📁 FILE   - Send as File format without Screenshots
+
+<b>If, You want to Set custom Thumbnail, Send photo before or quickly after tapping on any of the below buttons.\n\nYou can use /deletethumbnail to delete the auto-generated thumbnail.</b>"""
+    SET_CUSTOM_USERNAME_PASSWORD = "\n *Thank For UsiNg Me*"
+    NOYES_URL = "@robot URL detected. Please send me a fastest URL so that I can upload to Telegram, without slowing down for other users."
     DOWNLOAD_START = "🔰 DownloadiNg... to my Server ⬇️"
     UPLOAD_START = "⬆️ UploadiNg... to Telegram 📤 \n\n<b>🔄Processing:-</b>"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me, if you find me useful. Join : @Kalam_Company"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds \nUploaded in {} seconds \n\n Sorry, For Delay of Upload 😅"
-    AFTER_SUCCESSFUL_CANCEL_MSG_WITH_TS = "❌️ Process Cancelled ❌️"
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription..."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact @Kalam_Company"
     SAVED_CUSTOM_THUMB_NAIL = """✅ Custom Permanent 🖼 Thumbnail saved. This image will be used in your future video / file
@@ -37,22 +36,28 @@ URL | filename | username | password"""
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE = "@Kalam_url_Bot"
     NO_CUSTOM_THUMB_NAIL_FOUND = "❌️ No, Custom ThumbNail found."
-    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
+    NO_VOID_FORMAT_FOUND = "❌️ ERROR...\n {} \n\nPlease send another link but that link must contain any media"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     CURENT_PLAN_DETAILS = """Current plan details
 --------
 Telegram ID: <code>{}</code>
 Plan name: Free Cloned User
-Expires on: 31/12/2020"""
-    HELP_USER = """Hai, I am URL Uploader bot..
+Expires on: 01.06.2021
+
+<b>📝Note: It's Completely Free for my Channel members Only</b>"""
+    HELP_USER = """Hai, I am URL Uploader bot...
     
+<b>It's not very Complicated to Use Me!!! 😅
+
+Follow Steps Given Below 👇👇👇</b>
+
 1. Send url (Link | New Name of the file with Extension).
-2. Send Custom Thumbnail (Optional).
+2. Send your Custom Thumbnail (Optional).
 3. Select the button.
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File with Screenshots
-   Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots
+   SVIDEO - Send as Streaming Video format with 📸screenshots📸
+   VIDEO  - Send as Stream format without Screenshots
+   SFILE  - Send as File format with 📸Screenshots📸
+   FILE   - Send as File format without Screenshots
    
 --------
 
@@ -89,4 +94,3 @@ You can use /rename command after receiving file to rename it with custom thumbn
 Free users only 1 request per 30 minutes.
 /upgrade or Try 1800 seconds later."""
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file.  Please send me a fastest URL so that I can upload to Telegram, without me slowing down for other users."
-    ABOUT_TEXT = "Hii, Sexy"
