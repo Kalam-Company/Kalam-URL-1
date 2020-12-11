77fe08bde65f8eaaf55a03abee3c10cb4f13facd
Kiruthikvarma55 <75213972+Kiruthikvarma55@users.noreply.github.com>
2020-12-11 10:59:17 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -41,7 +41,7 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n\n🔄Percentage: {2}%\n".format(
+        progress = "[{0}{1}] \n\n⭕️Percentage: {2}%\n".format(
             ''.join(["🇮🇳" for i in range(math.floor(percentage / 10))]),
             ''.join(["🏳" for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
